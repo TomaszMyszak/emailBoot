bd4fa84dcdee58edf6cda936386ca9acba2525bd
Tomasz Myszak <76054684+TomaszMyszak@users.noreply.github.com>
2022-11-01 12:04:07 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boot2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //boot kopiujący zawartość stron internetowych do pliku txt
+
+            Console.WriteLine("Podaj adres strony internetowej");
+            string adres = Console.ReadLine();
+            Console.WriteLine("Podaj nazwę pliku");
+            string nazwa = Console.ReadLine();
+            Console.WriteLine("Podaj rozszerzenie pliku");
+            string rozszerzenie = Console.ReadLine();
+            string nazwaPliku = nazwa + "." + rozszerzenie;
+            string sciezka = "D:\\" + nazwaPliku;
+            System.Net.WebClient wc = new System.Net.WebClient();
+            wc.DownloadFile(adres, sciezka);
+            Console.WriteLine("Plik został zapisany na pulpicie");
+            Console.ReadKey();
+            
+        }
+    }
+}
